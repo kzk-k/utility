b0b3c5c0f41c08de1e18a723e3e5183af04bbd9a
k-k <kawano.kazuki0714@gmail.com>
2019-04-11 19:06:39 +0900

    babel追加

--- static_pug/dest/sp/jobs/script.js
+++ static_pug/dest/sp/jobs/script.js
@@ -1,26 +1,31 @@
-$(function() {
-	// interview
-	(function() {
-		var galleryThumbs = new Swiper('.js-swiper-interview-thum', {
-			spaceBetween: 8,
-			slidesPerView: 6,
-			// loop: true,
-			freeMode: true,
-			watchSlidesVisibility: true,
-			watchSlidesProgress: true
-		});
+"use strict";
 
-		var galleryTop = new Swiper('.js-swiper-interview-main', {
-			loop: true,
-			preloadImages: false,
-			lazyLoading: true,
-			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev',
-			},
-			thumbs: {
-				swiper: galleryThumbs
-			}
-		});
-	})();
+$(function () {
+  var test = function test() {
+    console.log('test');
+  }; // interview
+
+
+  (function () {
+    var galleryThumbs = new Swiper('.js-swiper-interview-thum', {
+      spaceBetween: 8,
+      slidesPerView: 6,
+      // loop: true,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true
+    });
+    var galleryTop = new Swiper('.js-swiper-interview-main', {
+      loop: true,
+      preloadImages: false,
+      lazyLoading: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      thumbs: {
+        swiper: galleryThumbs
+      }
+    });
+  })();
 });
